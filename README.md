6b02b5a93eb7fb318151f376f90ac2617fe6fa7a
sarainéscalderón <saraines.calderon@gmail.com>
2017-03-28 11:55:41 -0500

    updates

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Sara's basic weather app.
 start on 3000
 webpack
 start command: node server.js
+
+https://enigmatic-sierra-11014.herokuapp.com/
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -5,8 +5,8 @@ var Nav = () => {
 	return(
 			<div>
 				<h2>Nav</h2>
-				<IndexLink to='/' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-				<Link to ='/about' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+				<IndexLink to='/' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather | </IndexLink>
+				<Link to ='/about' activeClassName="active" activeStyle={{fontWeight: 'bold'}}> About | </Link>
 				<Link to ='/examples' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
 			</div>
 		);
